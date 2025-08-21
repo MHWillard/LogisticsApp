b55cfbb0351a1e35e348f7c3bd5ff619c2b856ae
MHWillard <mhwillard@gmail.com>
2025-08-21 11:09:18 -0400

    Test commit

--- LogisticsAppTests/UnitTests/Orders/OrderTests.cs
+++ LogisticsAppTests/UnitTests/Orders/OrderTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LogisticsApp.Models.Items;
+using LogisticsApp.Models.Orders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,6 +16,36 @@ namespace LogisticsAppTests.UnitTests.Orders
         {
             //arrange
             //database setup, object that we're looking for
+            Item Item1 = new Item
+            {
+                ItemId = 1,
+                ItemDetailId = 1,
+                OrderId = 1,
+                Quantity = 1,
+                Name = "item1",
+                Description = "item1dec"
+            };
+
+            Item Item2 = new Item
+            {
+                ItemId = 2,
+                ItemDetailId = 2,
+                OrderId = 1,
+                Quantity = 2,
+                Name = "item2",
+                Description = "item2dec"
+            };
+
+            Order Order1 = new Order
+            {
+                OrderId = 1,
+                Buyer = "Buyer",
+                Seller = "seller",
+                PurchaseDate = DateTime.Now,
+                ShippingDate = DateTime.Now,
+                DeliveryDate = DateTime.Now,
+                Items = 
+            }
 
             //act
             //query table object
